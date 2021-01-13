0a54a57c5650489f6ce5228d1c40d3a5512d9b01
HussainM3 <hussainmohd2003@outlook.com>
2021-01-13 15:01:32 -0500

    Changes to be committed:

---
+++ JuniorCCCPrepJ1_1.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+/**
+ * Date: Dec 2020
+ * @author Hussain
+ *	Description: J1: Dog Treats
+ */
+
+public class JuniorCCCPrepJ1_1 {
+	
+	public static void main(String[] args) {
+
+		// declare variables for input
+		int small;
+		int medium;
+		int large;
+		
+		// declare variable for score
+		int score;
+		
+		// prepare keyboard for scanner input 
+		Scanner input = new Scanner (System.in);
+		
+		// store the different inputs into each variable
+		small = input.nextInt();
+		
+		medium = input.nextInt();
+		
+		large = input.nextInt();
+
+		// close keyboard for scanner
+		input.close();
+		
+		// calculate happiness score
+		// input it into score variable
+		score = (1*small) + (2*medium) + (3*large);
+		
+		// output happy if score is 10 or higher
+		//otherwise output sad
+		if (score >= 10) {
+			System.out.print("happy");
+		}
+		
+		else {
+			System.out.print("sad");
+		}
+		
+		
+	}
+
+}
